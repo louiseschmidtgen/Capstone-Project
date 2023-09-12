5aa2eb087d01241ec935a35939e7acfce1a60ab4
louiseschmidtgen <louise.schmidtgen@gmail.com>
2023-09-12 15:54:36 -0600

    fix google API request

--- app/Google_API.py
+++ app/Google_API.py
@@ -1,91 +1,80 @@
 from Pop_up_gui import PopUpGUI
-import http.client
+import requests
 import json
-import sys
 import os
 
 
 class GoogleAPI():
     def __init__(self):
         self.popup = PopUpGUI()
+        self.api_host = str(os.getenv('X_RapidAPI_Host'))
+        self.api_key = str(os.getenv('X_RapidAPI_Key'))
 
     def get_e2g_translation(self, engl_word):
-        """This function calls the translate function to translate its input
-        Args:
-            engl_word (str): Users input that needs to be translated.
-
-        Returns:
-            str: The German translation of the Word.
-        """
-        return self.translate(engl_word, source_lang="en", target_lang="ger")
+        """This function calls the translate function to translate its input   """
+        return self.translate(engl_word, source_lang="en", target_lang="de")
 
     def get_g2e_translation(self, ger_word):
-        """This function calls the translate function to translate its input
-
-        Args:
-            ger_word (str): Users input that needs to be translated.
-
-        Returns:
-            str: The English translation of the Word.
-        """
-        return self.translate(ger_word, source_lang="ger", target_lang="en")
+        """This function calls the translate function to translate its input """
+        return self.translate(ger_word, source_lang="de", target_lang="en")
 
     def translate(self, word, source_lang, target_lang):
-        """This function calls Google API to translate a Word from a given source language to a target language.
+        """This function calls Google API to translate a Word from a given source language to a target language. """
+        if not self.api_host or not self.api_key:
+            print("Environment variables for Google API not set.")
+            return None
 
-        Args:
-            word (str): word that should be translated
-            source_lang (str): has either value ger/en
-            target_lang (str): has either value ger/en
-        Returns:
-            str: The translation of the Word.
-        """
-        # get keys from environment
-        try:
-            api_host = str(os.getenv('X_RapidAPI_Host'))
-            # This key is limited to 500 calls/month.
-            api_key = str(os.getenv('X_RapidAPI_Key'))
-        except Exception as e:
-            print(
-                "Check that API key and host is provided in the bash script run-main.sh.")
-            print("Oops!", sys.exc_info()[0], "occurred.")
-            print("Exception: ", e)
-            self.popup.createPopUp(
-                "Environment variables for Google API not set.")
-            sys.exit(1)  # crash
+        # url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
 
-        # establish connection to API host
-        conn = http.client.HTTPSConnection("google-translate1.p.rapidapi.com")
-        # Hello%2C%20world
-        payload = f"q={word}&target={target_lang}&source={source_lang}"
+        # payload = {
+        #     "source": source_lang,
+        #     "target": target_lang,
+        #     "q": word
+        # }
 
+        # headers = {
+        #     'content-type': "application/x-www-form-urlencoded",
+        #     'Accept-Encoding': "application/gzip",
+        #     'X-RapidAPI-Key': self.api_host,
+        #     'X-RapidAPI-Host': self.api_key
+        # }
+        url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
+
+        payload = {
+            "source": source_lang,
+            "target": target_lang,
+            "q": word
+        }
         headers = {
-            'content-type': "application/x-www-form-urlencoded",
-            'accept-encoding': "application/gzip",
-            'x-rapidapi-host': api_host,
-            'x-rapidapi-key': api_key
+            "content-type": "application/x-www-form-urlencoded",
+            "Accept-Encoding": "application/gzip",
+            "X-RapidAPI-Key": "9928c7260amshad0766390e03e89p1f4683jsn18824f37a5dc",
+            "X-RapidAPI-Host": "google-translate1.p.rapidapi.com"
         }
 
-        conn.request("POST", "/language/translate/v2", payload, headers)
-
-        res = conn.getresponse()
-
-        status = res.status  # 200 for is found #302 not found
-        print(f"status: {status}")
-
-        if status != 200:
-            print("Error with Google API: ")
-            self.popup.createPopUp(
-                f"Error with Google API: Status Code: {status}")
-            return ""
-        data = res.read()
-        # dump it in json # convert result to json/dict#dump it in json # convert result to json/dict
-        result_json = json.loads(data.decode("utf-8"))
-        print(result_json)
         try:
-            # get translation from result
-            translation = result_json["data"]["translations"][0]["translatedText"]
-        except IndexError:
-            print("Key error reading result.")
-        print(f"translation: {translation}")
-        return translation
+            print(">>>>>>>>>>>>>>", payload, headers)
+            response = requests.post(url, data=payload, headers=headers)
+            response.raise_for_status()
+            print(">>>>>>>>>>>>>>", response.text,
+                  response.status_code, response.url)
+            result_json = response.json()
+
+            translations = result_json.get("data", {}).get("translations", [])
+
+            if translations:
+                translation = translations[0]["translatedText"]
+                return translation
+            else:
+                print("Translation not found in response.")
+                return None
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error with Google API: {e}")
+            self.popup.createPopUp(f"Error with Google API: {e}")
+            return None
+
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON response: {e}")
+            self.popup.createPopUp(f"Error decoding JSON response: {e}")
+            return None
